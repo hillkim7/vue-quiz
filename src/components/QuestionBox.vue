8b01b48523e35c54f57b6ed689781626cd077948
hill <hillkim7@gmail.com>
2020-03-10 08:22:12 +0900

    Build question box

--- src/components/QuestionBox.vue
+++ src/components/QuestionBox.vue
@@ -1,3 +1,80 @@
 <template>
-    <h1>Question Box</h1>
+  <div class="question-box container">
+    <b-jumbotron>
+      <template v-slot:lead>
+        {{ currentQuestion.question }}
+      </template>
+
+      <hr class="my-4">
+
+      <b-list-group>
+      <b-list-group-item
+        v-for="(answer, index) in answers"
+        :key="index"
+        @click="selectAnswer(index)"
+        :class="[selectedIndex === index ? 'selected' : '']"
+        >
+        {{ answer }}
+      </b-list-group-item>
+      </b-list-group>
+      <b-button variant="primary" href="#">Submit</b-button>
+      <b-button @click="next" variant="success" href="#">Next</b-button>
+    </b-jumbotron>
+  </div>
 </template>
+
+<script>
+export default {
+  props: {
+    currentQuestion: Object,
+    next: Function
+  },
+  data() {
+    return {
+      selectedIndex: null
+    }
+  },
+  computed: {
+    answers() {
+      let answers = [...this.currentQuestion.incorrect_answers]
+      answers.push(this.currentQuestion.correct_answer)
+      return answers
+    }
+  },
+  methods: {
+    selectAnswer(index) {
+      this.selectedIndex = index
+      console.log("index:", this.selectedIndex)
+    }
+  },
+  mounted() {
+    console.log("currentQuestion:", this.currentQuestion)
+  }
+}
+</script>
+
+<style scoped>
+.list-group {
+  margin-bottom: 15px;
+}
+
+.list-group-item:hover {
+  background: #eee;
+}
+
+.btn {
+  margin: 0 5px;
+}
+
+.selected {
+  background-color: lightblue;
+}
+
+.correct {
+  background-color: lightgreen;
+}
+
+.incorrect {
+  background-color: yellow;
+}
+</style>
